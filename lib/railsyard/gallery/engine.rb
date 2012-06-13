4d72fe39f1f3ed4bd3733313342c75d9693f4df6
Stefano Verna <stefano.verna@welaika.com>
2012-06-13 18:22:33 +0200

    Only add default rails setup if necessary (also add dragonfly middleware)

--- lib/railsyard/gallery/engine.rb
+++ lib/railsyard/gallery/engine.rb
@@ -2,9 +2,21 @@ module Railsyard::Gallery
   class Engine < ::Rails::Engine
 
     initializer "Automatic require of Dragonfly" do |app|
+
       begin
+        # try to include dragonfly and railsyard-dragonfly
+        require 'dragonfly'
         require 'railsyard-dragonfly'
-        require 'dragonfly/rails/images'
+
+        # check if someone already initialized Dragonfly
+        # in this case we check if someone included dragonfly macros to ActiveRecord::Base
+        already_extended = (class << ActiveRecord::Base; self; end).included_modules.include?(Dragonfly::ActiveModelExtensions)
+        unless already_extended
+          require 'dragonfly/rails/images'
+        end
+
+        # in any case we add the Dragonfly[:images] app as middleware
+        app.config.middleware.insert 1, 'Dragonfly::Middleware', :images
       rescue LoadError
         puts "WARNING: railsyard-gallery needs railsyard-dragonfly in order to properly work! Add it to your Gemfile!"
       end
